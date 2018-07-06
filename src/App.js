d8a51d5ff46c7e06006a801d7b71974c0946d93a
Qi <qiwan@qiwan.usersys.redhat.com>
2018-07-06 15:00:24 -0400

    first initial

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  
+
+  render(){
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-l col-xs-offset-11">
+            <h1>hello !!</h1>
+          </div>
+        </div>
+      </div>
+      
+      
+      // document.getElementById('root')
+    );
+    
+  }
+}
+
+export default App;
+
