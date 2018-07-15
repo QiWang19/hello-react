9d7c3c1dc86db29dd35e3a4c1d258a50fff521fd
qwang12 <qwang12@wpi.edu>
2018-07-15 17:08:39 -0400

    bind

--- The Complete React Web Developer Course (with Redux)/r-04-05-jsx/helloReact.md
+++ The Complete React Web Developer Course (with Redux)/r-04-05-jsx/helloReact.md
@@ -311,4 +311,37 @@ constructor(props) {
 
 ### Component State  
 
-1. add stste to counter app
+1. add stste to counter app 
+  create funcions and bind 
+
+```javascript
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne= this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  handleAddOne() {
+    alert()
+  }
+  handleMinusOne() {
+
+  }
+  handleReset() {
+
+  } 
+  render() {
+    return (
+      <div>
+        <h1>Count: </h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Counter/>, document.getElementById('app'));
+```
--- The Complete React Web Developer Course (with Redux)/r-04-05-jsx/src/playground/counter-example.js
+++ The Complete React Web Developer Course (with Redux)/r-04-05-jsx/src/playground/counter-example.js
@@ -1,31 +1,61 @@
-let count = 0;
-const someId = 'myidhere'
-const addOne= ()=>{
-  count = count + 1;
-  renderCounterApp();
-  // console.log('addOne', count);
+// let count = 0;
+// const someId = 'myidhere'
+// const addOne= ()=>{
+//   count = count + 1;
+//   renderCounterApp();
+//   // console.log('addOne', count);
+// }
+
+// const minusOne = () => {
+//   count--;
+//   renderCounterApp();
+//   // console.log('minusOne');
+// }
+
+
+
+
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button id={someId} className="button" onClick={addOne}>
+//       +1
+//       </button>
+//       <button onClick={minusOne}>-1</button>
+//     </div>
+//   );
+//   ReactDOM.render(templateTwo, appRoot);
+// };
+
+// renderCounterApp();
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne= this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  handleAddOne() {
+    alert()
+  }
+  handleMinusOne() {
+
+  }
+  handleReset() {
+
+  } 
+  render() {
+    return (
+      <div>
+        <h1>Count: </h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
 }
 
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-  // console.log('minusOne');
-}
-
-
-
-
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button id={someId} className="button" onClick={addOne}>
-      +1
-      </button>
-      <button onClick={minusOne}>-1</button>
-    </div>
-  );
-  ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+ReactDOM.render(<Counter/>, document.getElementById('app'));
